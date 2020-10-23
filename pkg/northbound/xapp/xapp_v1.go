3569277571577420e1a6706f5de9d09359a9e535
Adib Rastegarnia <adib.rastegarnia@gmail.com>
2020-10-23 15:16:10 -0700

    Initial commit to add NB grpc servers (#14)

---
+++ pkg/northbound/xapp/xapp_v1.go
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package xapp
+
+import (
+	xappv1 "github.com/onosproject/onos-e2t/api/xapp/v1"
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+	"github.com/onosproject/onos-lib-go/pkg/northbound"
+	"google.golang.org/grpc"
+)
+
+var log = logging.GetLogger("northbound", "xapp")
+
+// Service is a Service implementation for E2T service.
+type Service struct {
+	northbound.Service
+}
+
+// Register registers the Service with the gRPC server.
+func (s Service) Register(r *grpc.Server) {
+	server := Server{}
+	xappv1.RegisterE2TServiceServer(r, server)
+}
+
+// Server implements the gRPC service for E2AP related functions.
+type Server struct {
+}
+
+// RegisterApp ...
+func (s Server) RegisterApp(xappv1.E2TService_RegisterAppServer) error {
+	log.Error("Implement me")
+	return nil
+}
