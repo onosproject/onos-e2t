40105500b91a94d4f788d1f7c366da798d1303bd
Ray Milkey <ray@opennetworking.org>
2020-10-29 10:14:07 -0700

    Implement skeletal CLI for onos-e2t (#34)
    * Initial pieces for a CLI to dump out the connections
    
    * Implement the 'get connections' command
    
    * gofmt

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang/protobuf v1.4.2
 	github.com/ishidawataru/sctp v0.0.0-20191218070446-00ab2ac2db07
 	github.com/onosproject/onos-lib-go v0.6.17
+	github.com/spf13/cobra v0.0.5
 	google.golang.org/grpc v1.27.1
 	gotest.tools v2.2.0+incompatible
 )
---
+++ pkg/cli/connections.go
@@ -0,0 +1,67 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package cli
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"text/tabwriter"
+
+	"github.com/onosproject/onos-e2t/api/admin/v1"
+	"github.com/onosproject/onos-lib-go/pkg/cli"
+	"github.com/spf13/cobra"
+)
+
+func getGetConnectionsCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "connections",
+		Short: "Get SB connections",
+		RunE:  runConnectionsCommand,
+	}
+	cmd.Flags().Bool("no-headers", false, "disables output headers")
+	return cmd
+}
+
+func runConnectionsCommand(cmd *cobra.Command, args []string) error {
+	noHeaders, _ := cmd.Flags().GetBool("no-headers")
+	conn, err := cli.GetConnection(cmd)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	outputWriter := cli.GetOutput()
+	writer := new(tabwriter.Writer)
+	writer.Init(outputWriter, 0, 0, 3, ' ', tabwriter.FilterHTML)
+
+	if !noHeaders {
+		_, _ = fmt.Fprintln(writer, "Global ID\tPLNM ID\tIP Addr\tPort")
+	}
+
+	request := admin.ListE2NodeConnectionsRequest{}
+
+	client := admin.CreateE2TAdminServiceClient(conn)
+
+	stream, err := client.ListE2NodeConnections(context.Background(), &request)
+	if err != nil {
+		return err
+	}
+
+	for {
+		response, err := stream.Recv()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			cli.Output("recv error")
+			return err
+		}
+
+		_, _ = fmt.Fprintf(writer, "%d\t%s\t%s\t%d\n", response.Id, response.PlmnId, response.RemoteIp, response.RemotePort)
+	}
+
+	_ = writer.Flush()
+
+	return nil
+}
