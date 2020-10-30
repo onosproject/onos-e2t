c6fb70b6c6548b00b30cc48cd733aa032ff8b5f4
Ivan Eroshkin <erijj@mail.ru>
2020-10-30 10:49:31 +0100

    [SDRAN-35] Adding RIC Subscription Response -- WIP (#35)
    * [SDRAN-35] Minor update on function names - for better distinguishing
    
    * [SDRAN-35] Adding Ric Supscription Response (incomplete part of protobuf)
    
    * Fixing linters issue

---
+++ pkg/southbound/sandbox/ric-subscription-response.go
@@ -0,0 +1,68 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+package sandbox
+
+import (
+	"fmt"
+	"github.com/onosproject/onos-e2t/api/e2ap/v1beta1/e2apies"
+	"github.com/onosproject/onos-e2t/api/e2ap/v1beta1/e2appducontents"
+	"github.com/onosproject/onos-e2t/api/e2ap/v1beta1/e2appdudescriptions"
+)
+
+const mask20bitricResponse = 0xFFFFF
+
+func CreateRicSubscriptionResponseE2apPdu(ricReqID int32, ricInstanceID int32, ranFuncID int32) (*e2appdudescriptions.E2ApPdu, error) {
+
+	if ricReqID|mask20bitricResponse > mask20bitricResponse {
+		return nil, fmt.Errorf("expecting 20 bit identifier for RIC. Got %0x", ricReqID)
+	}
+	if ricInstanceID|mask20bitricResponse > mask20bitricResponse {
+		return nil, fmt.Errorf("expecting 20 bit identifier for RIC. Got %0x", ricInstanceID)
+	}
+
+	ricRequestID := e2appducontents.RicsubscriptionResponseIes_RicsubscriptionResponseIes29{
+		Value: &e2apies.RicrequestId{
+			RicRequestorId: ricReqID,      // sequence from e2ap-v01.00.asn1:1126
+			RicInstanceId:  ricInstanceID, // sequence from e2ap-v01.00.asn1:1127
+		},
+	}
+
+	ranFunctionID := e2appducontents.RicsubscriptionResponseIes_RicsubscriptionResponseIes5{
+		Value: &e2apies.RanfunctionId{
+			Value: ranFuncID, // range of Integer from e2ap-v01.00.asn1:1050, value from line 1277
+		},
+	}
+
+	ricActionAdmit := e2appducontents.RicsubscriptionResponseIes_RicsubscriptionResponseIes17{
+		Value: &e2appducontents.RicactionAdmittedList{
+			Value: make([]*e2appducontents.RicactionAdmittedItemIes, 0),
+		},
+	}
+
+	ricActionNotAdmit := e2appducontents.RicsubscriptionResponseIes_RicsubscriptionResponseIes18{
+		Value: &e2appducontents.RicactionNotAdmittedList{
+			Value: make([]*e2appducontents.RicactionNotAdmittedItemIes, 0),
+		},
+	}
+
+	e2apPdu := e2appdudescriptions.E2ApPdu{
+		E2ApPdu: &e2appdudescriptions.E2ApPdu_SuccessfulOutcome{
+			SuccessfulOutcome: &e2appdudescriptions.SuccessfulOutcome{
+				ProcedureCode: &e2appdudescriptions.E2ApElementaryProcedures{
+					RicSubscription: &e2appdudescriptions.RicSubscription{
+						SuccessfulOutcome: &e2appducontents.RicsubscriptionResponse{
+							ProtocolIes: &e2appducontents.RicsubscriptionResponseIes{
+								E2ApProtocolIes29: &ricRequestID,      //RIC request ID
+								E2ApProtocolIes5:  &ranFunctionID,     //RAN function ID
+								E2ApProtocolIes17: &ricActionAdmit,    // RIC action Admitted items ---> EMPTY !!
+								E2ApProtocolIes18: &ricActionNotAdmit, // RIC action not Admitted items --> EMPTY !!
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return &e2apPdu, nil
+}
