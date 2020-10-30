f2dafe2540ca4d4a9a42a8734551be500ae3f1bc
eroshiva <erijj@mail.ru>
2020-10-30 10:18:14 +0100

    Fixing linters issue

--- pkg/southbound/sandbox/ric-subscription-response.go
+++ pkg/southbound/sandbox/ric-subscription-response.go
@@ -10,25 +10,25 @@ import (
 	"github.com/onosproject/onos-e2t/api/e2ap/v1beta1/e2appdudescriptions"
 )
 
-const mask20bitric_response = 0xFFFFF
+const mask20bitricResponse = 0xFFFFF
 
 func CreateRicSubscriptionResponseE2apPdu(ricReqID int32, ricInstanceID int32, ranFuncID int32) (*e2appdudescriptions.E2ApPdu, error) {
 
-	if ricReqID|mask20bitric_response > mask20bitric_response {
+	if ricReqID|mask20bitricResponse > mask20bitricResponse {
 		return nil, fmt.Errorf("expecting 20 bit identifier for RIC. Got %0x", ricReqID)
 	}
-	if ricInstanceID|mask20bitric_response > mask20bitric_response {
+	if ricInstanceID|mask20bitricResponse > mask20bitricResponse {
 		return nil, fmt.Errorf("expecting 20 bit identifier for RIC. Got %0x", ricInstanceID)
 	}
 
-	ricRequestId := e2appducontents.RicsubscriptionResponseIes_RicsubscriptionResponseIes29{
+	ricRequestID := e2appducontents.RicsubscriptionResponseIes_RicsubscriptionResponseIes29{
 		Value: &e2apies.RicrequestId{
 			RicRequestorId: ricReqID,      // sequence from e2ap-v01.00.asn1:1126
 			RicInstanceId:  ricInstanceID, // sequence from e2ap-v01.00.asn1:1127
 		},
 	}
 
-	ranFunctionId := e2appducontents.RicsubscriptionResponseIes_RicsubscriptionResponseIes5{
+	ranFunctionID := e2appducontents.RicsubscriptionResponseIes_RicsubscriptionResponseIes5{
 		Value: &e2apies.RanfunctionId{
 			Value: ranFuncID, // range of Integer from e2ap-v01.00.asn1:1050, value from line 1277
 		},
@@ -53,8 +53,8 @@ func CreateRicSubscriptionResponseE2apPdu(ricReqID int32, ricInstanceID int32, r
 					RicSubscription: &e2appdudescriptions.RicSubscription{
 						SuccessfulOutcome: &e2appducontents.RicsubscriptionResponse{
 							ProtocolIes: &e2appducontents.RicsubscriptionResponseIes{
-								E2ApProtocolIes29: &ricRequestId,      //RIC request ID
-								E2ApProtocolIes5:  &ranFunctionId,     //RAN function ID
+								E2ApProtocolIes29: &ricRequestID,      //RIC request ID
+								E2ApProtocolIes5:  &ranFunctionID,     //RAN function ID
 								E2ApProtocolIes17: &ricActionAdmit,    // RIC action Admitted items ---> EMPTY !!
 								E2ApProtocolIes18: &ricActionNotAdmit, // RIC action not Admitted items --> EMPTY !!
 							},
