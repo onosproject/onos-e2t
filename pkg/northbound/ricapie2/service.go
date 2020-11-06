0690257b632cba3120714a75d4f02c279ac80545
Jordan Halterman <jordan.halterman@gmail.com>
2020-11-06 14:12:07 -0800

    Extend northbound API to use streams

--- pkg/northbound/ricapie2/service.go
+++ pkg/northbound/ricapie2/service.go
@@ -5,6 +5,9 @@
 package ricapie2
 
 import (
+	"fmt"
+	"github.com/onosproject/onos-e2t/api/ricapi/e2/headers/v1beta1"
+	"github.com/onosproject/onos-e2t/pkg/northbound/stream"
 	"io"
 
 	ricapie2v1beta1 "github.com/onosproject/onos-e2t/api/ricapi/e2/v1beta1"
@@ -15,43 +18,72 @@ import (
 
 var log = logging.GetLogger("northbound", "ricapi", "e2")
 
+// NewService creates a new E2T service
+func NewService(streams *stream.Manager) northbound.Service {
+	return &Service{streams: streams}
+}
+
 // Service is a Service implementation for E2T service.
 type Service struct {
 	northbound.Service
+	streams *stream.Manager
 }
 
 // Register registers the Service with the gRPC server.
 func (s Service) Register(r *grpc.Server) {
-	server := Server{}
+	server := &Server{streams: s.streams}
 	ricapie2v1beta1.RegisterE2TServiceServer(r, server)
 
 }
 
 // Server implements the gRPC service for E2 ricapi related functions.
 type Server struct {
+	streams *stream.Manager
 }
 
-// RegisterApp process and handle the incoming requests from xApps
-func (s Server) RegisterApp(stream ricapie2v1beta1.E2TService_RegisterAppServer) error {
-	ctx := stream.Context()
+func (s *Server) Stream(server ricapie2v1beta1.E2TService_StreamServer) error {
+	// Get the application name
+	request, err := server.Recv()
+	if err == io.EOF {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	log.Debugf("Received StreamRequest %+v", request)
+	streamID := stream.ID(fmt.Sprintf("%s:%s", request.AppID, request.InstanceID))
+	streamCh := make(chan stream.Message)
+	stream, err := s.streams.Open(server.Context(), streamID, streamCh)
+	if err != nil {
+		log.Warnf("StreamRequest %+v failed: %v", request, err)
+		return err
+	}
+
 	for {
-
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-		}
-
-		req, err := stream.Recv()
+		message, err := stream.Recv()
 		if err == io.EOF {
-			log.Error("End of file error", err, req)
 			return nil
 		}
 		if err != nil {
-			log.Error(err)
-			continue
+			log.Warnf("StreamRequest %+v failed: %v", request, err)
+			return err
 		}
 
-	}
+		response := &ricapie2v1beta1.StreamResponse{
+			Header: &v1beta1.ResponseHeader{
+				EncodingType: v1beta1.EncodingType_ENCODING_TYPE_PROTO,
+			},
+			Payload: message.Payload,
+		}
 
+		err = server.Send(response)
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Warnf("StreamResponse %+v failed: %v", response, err)
+			return err
+		}
+	}
 }
