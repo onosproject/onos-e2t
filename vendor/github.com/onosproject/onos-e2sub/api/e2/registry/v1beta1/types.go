331fd404e04dfb9daba0eee3dc370b5d6a1b3cbc
Ray Milkey <ray@opennetworking.org>
2020-11-11 09:14:10 -0800

    Enable vendoring on build (#86)

---
+++ vendor/github.com/onosproject/onos-e2sub/api/e2/registry/v1beta1/types.go
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package v1beta1
+
+// ID is a device ID
+type ID string
+
+// IP is encoded as a string
+type IP string
+
+type Port int32
