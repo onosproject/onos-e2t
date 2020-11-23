2c227b065d98aa7bcac44710b64d38d4051172cd
Jordan Halterman <jordan.halterman@gmail.com>
2020-11-23 08:06:52 -0800

    Add E2 protocol (#120)
    * Add SCTP protocol client and server
    
    * Fix linter error in SCTP server
    
    * Add E2 connection interface
    
    * Remove E2 connection
    
    * Add procedures for E2 protocol
    
    * Add E2 channels
    
    * Add client/server for E2 protocol
    
    * Add initial channel test
    
    * Add async utilities
    
    * Remove unused channel function
    
    * Run E2 request-response procedures in a new goroutine
    
    * Serialize indications in a separate goroutine to avoid blocking E2 channels
    
    * Return synchronous errors when sending on the thread safe channel

---
+++ pkg/utils/async/async_test.go
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package async
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestApply(t *testing.T) {
+	values := []string{
+		"one",
+		"two",
+		"three",
+	}
+	err := Apply(len(values), func(i int) error {
+		return nil
+	})
+	assert.NoError(t, err)
+}
+
+func TestExecute(t *testing.T) {
+	values := []string{
+		"one",
+		"two",
+		"three",
+	}
+	results, err := Execute(len(values), func(i int) (interface{}, error) {
+		return values[i], nil
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, results)
+	assert.NotEqual(t, "", results[0].(string))
+	assert.NotEqual(t, "", results[1].(string))
+	assert.NotEqual(t, "", results[2].(string))
+}
