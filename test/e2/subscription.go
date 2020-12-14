d3724195f295e3eb028660e2ca1d23e1c8367e7e
Adib Rastegarnia <adib.rastegarnia@gmail.com>
2020-12-14 14:44:31 -0800

    Fix integration test (#144)
    * Fix integration test
    
    * Fix linter
    
    * use name for simulator chart
    
    * minor issue

--- test/e2/subscription.go
+++ test/e2/subscription.go
@@ -6,9 +6,11 @@ package e2
 
 import (
 	"context"
-	"github.com/onosproject/onos-api/go/onos/e2sub/subscription"
 	"testing"
 	"time"
+
+	"github.com/onosproject/onos-api/go/onos/e2sub/subscription"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/onosproject/onos-ric-sdk-go/pkg/e2/indication"
 
@@ -16,6 +18,7 @@ import (
 
 	e2client "github.com/onosproject/onos-ric-sdk-go/pkg/e2"
 
+	"github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm/pdubuilder"
 	"github.com/onosproject/onos-e2t/test/utils"
 )
 
@@ -24,8 +27,31 @@ const (
 	SubscriptionServicePort = 5150
 )
 
+func createEventTriggerTestData() ([]byte, error) {
+
+	var rtPeriod int32 = 12
+	e2SmKpmEventTriggerDefinition, err := pdubuilder.CreateE2SmKpmEventTriggerDefinition(int32(rtPeriod))
+	if err != nil {
+		return []byte{}, err
+	}
+	err = e2SmKpmEventTriggerDefinition.Validate()
+	if err != nil {
+		return []byte{}, err
+	}
+	protoBytes, err := proto.Marshal(e2SmKpmEventTriggerDefinition)
+	if err != nil {
+		return []byte{}, err
+	}
+	return protoBytes, nil
+}
+
 func createSubscriptionRequest(nodeID string) (subscription.SubscriptionDetails, error) {
-	return subscription.SubscriptionDetails{
+	eventTriggerTestData, err := createEventTriggerTestData()
+	if err != nil {
+		return subscription.SubscriptionDetails{}, err
+	}
+
+	subReq := subscription.SubscriptionDetails{
 		E2NodeID: subscription.E2NodeID(nodeID),
 		ServiceModel: subscription.ServiceModel{
 			ID: subscription.ServiceModelID("test"),
@@ -33,7 +59,7 @@ func createSubscriptionRequest(nodeID string) (subscription.SubscriptionDetails,
 		EventTrigger: subscription.EventTrigger{
 			Payload: subscription.Payload{
 				Encoding: subscription.Encoding_ENCODING_PROTO,
-				Data:     []byte{},
+				Data:     eventTriggerTestData,
 			},
 		},
 		Actions: []subscription.Action{
@@ -46,12 +72,14 @@ func createSubscriptionRequest(nodeID string) (subscription.SubscriptionDetails,
 				},
 			},
 		},
-	}, nil
+	}
+
+	return subReq, nil
 }
 
 // TestSubscription
 func (s *TestSuite) TestSubscription(t *testing.T) {
-	utils.CreateE2SimulatorWithName(t, "e2-simulator")
+	utils.CreateRanSimulatorWithName(t, "ran-simulator")
 
 	clientConfig := e2client.Config{
 		AppID: "subscription-test",
