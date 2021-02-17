58b8d6fd675e7f2c83d6f85e541791f746782be7
Sean Condon <sean@opennetworking.org>
2021-02-17 09:02:00 +0000

    Minor fix to E2AP 1.0.1 proto (#184)

--- api/e2ap/v1beta2/e2ap-pdu-contents/e2ap_pdu_contents.pb.validate.go
+++ api/e2ap/v1beta2/e2ap-pdu-contents/e2ap_pdu_contents.pb.validate.go
@@ -7144,6 +7144,16 @@ func (m *RanfunctionIdcauseItemIes) Validate() error {
 		return nil
 	}
 
+	if v, ok := interface{}(m.GetRanFunctionIdcauseItemIes7()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return RanfunctionIdcauseItemIesValidationError{
+				field:  "RanFunctionIdcauseItemIes7",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	return nil
 }
 
@@ -15618,6 +15628,113 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = RicserviceUpdateAcknowledgeIes_RicserviceUpdateAcknowledgeIes13ValidationError{}
+
+// Validate checks the field values on
+// RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7 with the rules defined
+// in the proto definition for this message. If any rules are violated, an
+// error is returned.
+func (m *RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	if m.GetId() != 7 {
+		return RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError{
+			field:  "Id",
+			reason: "value must equal 7",
+		}
+	}
+
+	if m.GetCriticality() != 1 {
+		return RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError{
+			field:  "Criticality",
+			reason: "value must equal 1",
+		}
+	}
+
+	if v, ok := interface{}(m.GetValue()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError{
+				field:  "Value",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if m.GetPresence() != 2 {
+		return RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError{
+			field:  "Presence",
+			reason: "value must equal 2",
+		}
+	}
+
+	return nil
+}
+
+// RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError is the
+// validation error returned by
+// RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7.Validate if the
+// designated constraints aren't met.
+type RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError) Field() string {
+	return e.field
+}
+
+// Reason function returns reason value.
+func (e RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError) Reason() string {
+	return e.reason
+}
+
+// Cause function returns cause value.
+func (e RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError) Cause() error {
+	return e.cause
+}
+
+// Key function returns key value.
+func (e RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError) ErrorName() string {
+	return "RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sRanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = RanfunctionIdcauseItemIes_RanfunctionIdcauseItemIes7ValidationError{}
 
 // Validate checks the field values on
 // RicserviceUpdateFailureIes_RicserviceUpdateFailureIes13 with the rules
