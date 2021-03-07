c067fca48428f4f99c67c634a3b9b2b5f11262b6
adibrastegarnia <adib.rastegarnia@gmail.com>
2021-03-07 15:46:45 -0800

    add an integration test for control

--- test/utils/subscription.go
+++ test/utils/subscription.go
@@ -9,6 +9,7 @@ import (
 
 	subapi "github.com/onosproject/onos-api/go/onos/e2sub/subscription"
 	"github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm/pdubuilder"
+	rcpdubuilder "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_rc_pre/pdubuilder"
 	"github.com/onosproject/onos-ric-sdk-go/pkg/e2/creds"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -27,7 +28,24 @@ type Subscription struct {
 	EventTrigger         []byte
 }
 
-// CreateKpmEventTrigger creates a kpm event trigger
+// CreateRcEventTrigger creates a rc service model event trigger
+func CreateRcEventTrigger() ([]byte, error) {
+	e2SmKpmEventTriggerDefinition, err := rcpdubuilder.CreateE2SmRcPreEventTriggerDefinitionUponChange()
+	if err != nil {
+		return []byte{}, err
+	}
+	err = e2SmKpmEventTriggerDefinition.Validate()
+	if err != nil {
+		return []byte{}, err
+	}
+	protoBytes, err := proto.Marshal(e2SmKpmEventTriggerDefinition)
+	if err != nil {
+		return []byte{}, err
+	}
+	return protoBytes, nil
+}
+
+// CreateKpmEventTrigger creates a kpm service model event trigger
 func CreateKpmEventTrigger(rtPeriod int32) ([]byte, error) {
 	e2SmKpmEventTriggerDefinition, err := pdubuilder.CreateE2SmKpmEventTriggerDefinition(rtPeriod)
 	if err != nil {
