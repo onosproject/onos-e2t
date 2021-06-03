99703fc07b32f1106b471e74a0902ab2792c8d20
Jordan Halterman <jordan.halterman@gmail.com>
2021-06-03 09:29:45 -0700

    Update onos-api dependency

--- test/utils/clients.go
+++ test/utils/clients.go
@@ -10,7 +10,6 @@ import (
 
 	e2client "github.com/onosproject/onos-ric-sdk-go/pkg/e2"
 	sdksub "github.com/onosproject/onos-ric-sdk-go/pkg/e2/subscription"
-	e2v1beta1client "github.com/onosproject/onos-ric-sdk-go/pkg/e2/v1beta1"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -32,18 +31,6 @@ func GetE2Client(t *testing.T, appID string) e2client.Client {
 	assert.NotNil(t, client)
 	return client
 
-}
-
-// GetE2V1beta1Client gets an E2 v1beta1 client
-func GetE2V1beta1Client(t *testing.T, appID string, instanceID string, serviceModelName, serviceModelVersion string) e2v1beta1client.Client {
-	return e2v1beta1client.NewClient(
-		e2v1beta1client.WithAppID(e2v1beta1client.AppID(appID)),
-		e2v1beta1client.WithInstanceID(e2v1beta1client.InstanceID(instanceID)),
-		e2v1beta1client.WithE2THost(E2TServiceHost),
-		e2v1beta1client.WithE2TPort(E2TServicePort),
-		e2v1beta1client.WithServiceModel(
-			e2v1beta1client.ServiceModelName(serviceModelName),
-			e2v1beta1client.ServiceModelVersion(serviceModelVersion)))
 }
 
 // getSubClient returns an SDK subscription client
