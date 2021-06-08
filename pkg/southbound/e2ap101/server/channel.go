5ea47aa4f8d52d72ccefff6345bf6346a3c7f22a
Ray Milkey <ray@opennetworking.org>
2021-06-08 14:23:25 -0700

    Revert "Implement v1beta1 control/subscription API servers and controllers (#291)"
    This reverts commit 34e1449f302f1589b164a3e99d92113cec0326de.

--- pkg/southbound/e2ap101/server/channel.go
+++ pkg/southbound/e2ap101/server/channel.go
@@ -6,7 +6,6 @@ package server
 
 import (
 	"context"
-	subscriptionv1beta1 "github.com/onosproject/onos-e2t/pkg/broker/subscription/v1beta1"
 
 	"github.com/onosproject/onos-e2t/pkg/broker/subscription"
 
@@ -14,34 +13,27 @@ import (
 	e2 "github.com/onosproject/onos-e2t/pkg/protocols/e2ap101"
 )
 
-func NewE2Channel(id ChannelID, plmnID string, channel e2.ServerChannel, streams subscription.Broker, streamsv1beta1 subscriptionv1beta1.Broker) *E2Channel {
+func NewE2Channel(id ChannelID, plmnID string, channel e2.ServerChannel, streams subscription.Broker) *E2Channel {
 	return &E2Channel{
-		ServerChannel:  channel,
-		ID:             id,
-		PlmnID:         plmnID,
-		streams:        streams,
-		streamsv1beta1: streamsv1beta1,
+		ServerChannel: channel,
+		ID:            id,
+		PlmnID:        plmnID,
+		streams:       streams,
 	}
 }
 
 type E2Channel struct {
 	e2.ServerChannel
-	ID             ChannelID
-	PlmnID         string
-	streams        subscription.Broker
-	streamsv1beta1 subscriptionv1beta1.Broker
+	ID      ChannelID
+	PlmnID  string
+	streams subscription.Broker
 }
 
 func (c *E2Channel) ricIndication(ctx context.Context, request *e2appducontents.Ricindication) error {
-	streamID := subscriptionv1beta1.StreamID(request.ProtocolIes.E2ApProtocolIes29.Value.RicRequestorId)
-	stream, ok := c.streamsv1beta1.GetWriter(streamID)
-	if !ok {
-		deprecatedStreamID := subscription.StreamID(request.ProtocolIes.E2ApProtocolIes29.Value.RicRequestorId)
-		deprecatedStream, err := c.streams.GetStream(deprecatedStreamID)
-		if err != nil {
-			return err
-		}
-		return deprecatedStream.Send(request)
+	streamID := subscription.StreamID(request.ProtocolIes.E2ApProtocolIes29.Value.RicRequestorId)
+	stream, err := c.streams.GetStream(streamID)
+	if err != nil {
+		return err
 	}
 	return stream.Send(request)
 }
