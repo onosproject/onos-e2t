81547a7f861bb3d4087bc29bbb5a0ad3bbb915dc
Ray Milkey <ray@opennetworking.org>
2021-06-22 15:35:00 -0700

    Port multiple reports subscription test to the new SDK API (#329)
    * Port multiple reports subscription test to the new SDK API
    
    * rename  SDK to e2api

--- test/e2/subscription_multiple_reports.go
+++ test/e2/subscription_multiple_reports.go
@@ -6,6 +6,7 @@ package e2
 
 import (
 	"context"
+	sdkclient "github.com/onosproject/onos-ric-sdk-go/pkg/e2/v1beta1"
 	"testing"
 
 	"github.com/onosproject/onos-e2t/test/e2utils"
@@ -13,9 +14,8 @@ import (
 	e2smkpmv2 "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm_v2/v2/e2sm-kpm-v2"
 	"google.golang.org/protobuf/proto"
 
-	subapi "github.com/onosproject/onos-api/go/onos/e2sub/subscription"
+	e2api "github.com/onosproject/onos-api/go/onos/e2t/e2/v1beta1"
 
-	"github.com/onosproject/onos-ric-sdk-go/pkg/e2/indication"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/onosproject/onos-e2t/test/utils"
@@ -26,9 +26,6 @@ func (s *TestSuite) TestSubscriptionMultipleReports(t *testing.T) {
 	sim := utils.CreateRanSimulatorWithNameOrDie(t, s.c, "subscription-multiple-reports")
 	assert.NotNil(t, sim)
 
-	e2Client := utils.GetE2Client(t, "subscription-multiple-reports")
-
-	ch := make(chan indication.Indication)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -56,66 +53,59 @@ func (s *TestSuite) TestSubscriptionMultipleReports(t *testing.T) {
 	actionDefinitionBytes1, err := utils.CreateKpmV2ActionDefinition(cellObjectIDList[1], 1000)
 	assert.NoError(t, err)
 
-	var actions []subapi.Action
-	action0 := subapi.Action{
+	var actions []e2api.Action
+	action0 := e2api.Action{
 		ID:   100,
-		Type: subapi.ActionType_ACTION_TYPE_REPORT,
-		SubsequentAction: &subapi.SubsequentAction{
-			Type:       subapi.SubsequentActionType_SUBSEQUENT_ACTION_TYPE_CONTINUE,
-			TimeToWait: subapi.TimeToWait_TIME_TO_WAIT_ZERO,
+		Type: e2api.ActionType_ACTION_TYPE_REPORT,
+		SubsequentAction: &e2api.SubsequentAction{
+			Type:       e2api.SubsequentActionType_SUBSEQUENT_ACTION_TYPE_CONTINUE,
+			TimeToWait: e2api.TimeToWait_TIME_TO_WAIT_ZERO,
 		},
-		Payload: subapi.Payload{
-			Encoding: subapi.Encoding_ENCODING_PROTO,
-			Data:     actionDefinitionBytes0,
-		},
+		Payload: actionDefinitionBytes0,
 	}
 
-	action1 := subapi.Action{
+	action1 := e2api.Action{
 		ID:   101,
-		Type: subapi.ActionType_ACTION_TYPE_REPORT,
-		SubsequentAction: &subapi.SubsequentAction{
-			Type:       subapi.SubsequentActionType_SUBSEQUENT_ACTION_TYPE_CONTINUE,
-			TimeToWait: subapi.TimeToWait_TIME_TO_WAIT_ZERO,
+		Type: e2api.ActionType_ACTION_TYPE_REPORT,
+		SubsequentAction: &e2api.SubsequentAction{
+			Type:       e2api.SubsequentActionType_SUBSEQUENT_ACTION_TYPE_CONTINUE,
+			TimeToWait: e2api.TimeToWait_TIME_TO_WAIT_ZERO,
 		},
-		Payload: subapi.Payload{
-			Encoding: subapi.Encoding_ENCODING_PROTO,
-			Data:     actionDefinitionBytes1,
-		},
+		Payload: actionDefinitionBytes1,
 	}
 
 	actions = append(actions, action0)
 	actions = append(actions, action1)
 
-	subRequest := utils.Subscription{
+	subRequest := utils.Subscription2{
 		NodeID:              string(nodeID),
-		EncodingType:        subapi.Encoding_ENCODING_PROTO,
 		EventTrigger:        eventTriggerBytes,
 		ServiceModelName:    utils.KpmServiceModelName,
 		ServiceModelVersion: utils.Version2,
 		Actions:             actions,
 	}
 
-	subReq, err := subRequest.CreateWithActionDefinition()
+	subSpec, err := subRequest.CreateWithActionDefinition2()
 	assert.NoError(t, err)
 
-	sub, err := e2Client.Subscribe(ctx, subReq, ch)
+	sdkClient := utils.GetE2Client2(t, utils.KpmServiceModelName, utils.Version2, sdkclient.ProtoEncoding)
+	node := sdkClient.Node(sdkclient.NodeID(nodeID))
+	ch := make(chan e2api.Indication)
+	_, err = node.Subscribe(ctx, "TestSubscriptionKpmV2", subSpec, ch)
 	assert.NoError(t, err)
 
 	indicationMessage := e2smkpmv2.E2SmKpmIndicationMessage{}
 	indicationHeader := e2smkpmv2.E2SmKpmIndicationHeader{}
 
 	for i := 0; i < 2; i++ {
-		indicationReport := e2utils.CheckIndicationMessage(t, e2utils.DefaultIndicationTimeout, ch)
-		err = proto.Unmarshal(indicationReport.Payload.Message, &indicationMessage)
+		indicationReport := e2utils.CheckIndicationMessage2(t, e2utils.DefaultIndicationTimeout, ch)
+		err = proto.Unmarshal(indicationReport.Payload, &indicationMessage)
 		assert.NoError(t, err)
-		err = proto.Unmarshal(indicationReport.Payload.Header, &indicationHeader)
+		err = proto.Unmarshal(indicationReport.Header, &indicationHeader)
 		assert.NoError(t, err)
 		cellObjectID := indicationMessage.GetIndicationMessageFormat1().GetCellObjId().Value
 		assert.True(t, cellObjectID == cellObjectIDList[0] || cellObjectID == cellObjectIDList[1])
 	}
-
-	err = sub.Close()
-	assert.NoError(t, err)
 
 	err = sim.Uninstall()
 	assert.NoError(t, err)
