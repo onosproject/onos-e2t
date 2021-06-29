b94c232b2afc61389875949db67b0f6642f1013f
Thomas Vachuska <tom@onlab.us>
2021-06-29 16:38:56 -0700

    Bumping onos-cli and ran-simulator chart deps (#356)

--- pkg/southbound/e2ap101/server/channel.go
+++ pkg/southbound/e2ap101/server/channel.go
@@ -6,7 +6,10 @@ package server
 
 import (
 	"context"
+	"github.com/onosproject/onos-api/go/onos/topo"
 	subscriptionv1beta1 "github.com/onosproject/onos-e2t/pkg/broker/subscription/v1beta1"
+	"github.com/onosproject/onos-e2t/pkg/southbound/e2ap101/types"
+	"time"
 
 	"github.com/onosproject/onos-e2t/pkg/broker/subscription"
 
@@ -14,11 +17,20 @@ import (
 	e2 "github.com/onosproject/onos-e2t/pkg/protocols/e2ap101"
 )
 
-func NewE2Channel(id ChannelID, plmnID string, channel e2.ServerChannel, streams subscription.Broker, streamsv1beta1 subscriptionv1beta1.Broker) *E2Channel {
+func NewE2Channel(id ChannelID, plmnID string, nodeID *types.E2NodeIdentity, channel e2.ServerChannel,
+	streams subscription.Broker, streamsv1beta1 subscriptionv1beta1.Broker, now time.Time) *E2Channel {
+	nid, err := GetNodeID(nodeID.NodeIdentifier)
+	if err != nil {
+		log.Warn("Unable to parse node ID: %v due to %v", nodeID, err)
+		nid = topo.ID(id)
+	}
 	return &E2Channel{
 		ServerChannel:  channel,
 		ID:             id,
 		PlmnID:         plmnID,
+		NodeID:         string(nid),
+		NodeType:       nodeID.NodeType,
+		TimeAlive:      now,
 		streams:        streams,
 		streamsv1beta1: streamsv1beta1,
 	}
@@ -27,7 +39,10 @@ func NewE2Channel(id ChannelID, plmnID string, channel e2.ServerChannel, streams
 type E2Channel struct {
 	e2.ServerChannel
 	ID             ChannelID
+	NodeID         string
+	NodeType       types.E2NodeType
 	PlmnID         string
+	TimeAlive      time.Time
 	streams        subscription.Broker
 	streamsv1beta1 subscriptionv1beta1.Broker
 }
