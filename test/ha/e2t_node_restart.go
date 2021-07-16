a785f325b6db3e7a3de84c9e65a95fa40e8ac6de
Jordan Halterman <jordan.halterman@gmail.com>
2021-07-16 00:12:57 -0700

    Refactor test suites to move HA tests to separate suite

--- test/ha/e2t_node_restart.go
+++ test/ha/e2t_node_restart.go
@@ -2,12 +2,14 @@
 //
 // SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
 
-package e2
+package ha
 
 import (
 	"context"
+	"github.com/onosproject/helmit/pkg/kubernetes"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/onosproject/onos-api/go/onos/e2t/e2/v1beta1"
 	e2api "github.com/onosproject/onos-api/go/onos/e2t/e2/v1beta1"
@@ -19,11 +21,10 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-// TestE2NodeRestart checks that a subscription channel read times out if
-// the e2 node is down.
-func (s *TestSuite) TestE2NodeRestart(t *testing.T) {
+// TestE2TNodeRestart checks that a subscription recovers after an E2T node restart
+func (s *TestSuite) TestE2TNodeRestart(t *testing.T) {
 	// Create a simulator
-	sim := utils.CreateRanSimulatorWithNameOrDie(t, s.c, "e2node-restart-subscription")
+	sim := utils.CreateRanSimulatorWithNameOrDie(t, s.c, "e2tnode-restart-subscription")
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -105,7 +106,7 @@ func (s *TestSuite) TestE2NodeRestart(t *testing.T) {
 	subSpec, err := subRequest.CreateWithActionDefinition()
 	assert.NoError(t, err)
 
-	subName := "TestSubscriptionKpmV2"
+	subName := "TestE2TNodeRestart"
 
 	sdkClient := utils.GetE2Client(t, utils.KpmServiceModelName, utils.Version2, sdkclient.ProtoEncoding)
 	node := sdkClient.Node(sdkclient.NodeID(nodeID))
@@ -125,12 +126,17 @@ func (s *TestSuite) TestE2NodeRestart(t *testing.T) {
 	err = proto.Unmarshal(indicationReport.Header, &indicationHeader)
 	assert.NoError(t, err)
 
-	t.Log("Restart e2 node")
-	_ = sim.Uninstall()
-	_ = sim.Install(true)
+	t.Log("Restart e2t node")
+	client, err := kubernetes.NewForRelease(s.release)
+	assert.NoError(t, err)
+	pods, err := client.CoreV1().Pods().List(ctx)
+	assert.NoError(t, err)
+	assert.NotZero(t, len(pods))
+	pod := pods[0]
+	assert.NoError(t, pod.Delete(ctx))
 
 	t.Log("Check indications")
-	indicationReport = e2utils.CheckIndicationMessage(t, e2utils.DefaultIndicationTimeout, ch)
+	indicationReport = e2utils.CheckIndicationMessage(t, time.Minute, ch)
 	indicationMessage = e2smkpmv2.E2SmKpmIndicationMessage{}
 	indicationHeader = e2smkpmv2.E2SmKpmIndicationHeader{}
 
