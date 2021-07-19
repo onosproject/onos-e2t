b1381e5a32bd8eda4e4f624098ac998ad67190f4
Thomas Vachuska <tom@opennetworking.org>
2021-07-19 15:29:57 -0700

    Using new relation ID scheme

--- pkg/topo/utils.go
+++ pkg/topo/utils.go
@@ -6,15 +6,8 @@ package topo
 
 import (
 	"os"
-
-	topoapi "github.com/onosproject/onos-api/go/onos/topo"
 )
 
 func getPodID() string {
 	return os.Getenv("POD_ID")
-}
-
-func getE2CellRelationID(deviceID topoapi.ID, cellID topoapi.ID) (topoapi.ID, error) {
-	cellRelationID := deviceID + "-" + cellID
-	return cellRelationID, nil
 }
