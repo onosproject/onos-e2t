a3b4173cb7a4c1f2f60b1cbd4756ecfa8ceb7162
adibrastegarnia <adib.rastegarnia@gmail.com>
2021-08-23 09:19:40 -0700

    Add E2AP v2 procedures to protocols package

---
+++ pkg/protocols/e2ap201/client.go
@@ -0,0 +1,46 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package e2
+
+import (
+	"context"
+
+	"github.com/onosproject/onos-lib-go/pkg/sctp/addressing"
+	"github.com/onosproject/onos-lib-go/pkg/sctp/types"
+
+	"github.com/onosproject/onos-e2t/pkg/protocols/e2ap201/channels"
+	"github.com/onosproject/onos-e2t/pkg/protocols/e2ap201/procedures"
+
+	sctp "github.com/onosproject/onos-lib-go/pkg/sctp"
+)
+
+// ClientHandler is a client handler function
+type ClientHandler func(channel ClientChannel) ClientInterface
+
+// ClientInterface is an interface for E2 client procedures
+type ClientInterface procedures.E2NodeProcedures
+
+// ClientChannel is an interface for initiating client procedures
+type ClientChannel channels.E2NodeChannel
+
+// Connect connects to the given address
+func Connect(ctx context.Context, address string, handler ClientHandler) (ClientChannel, error) {
+	addr, err := addressing.ResolveAddress(types.Sctp4, address)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := sctp.DialSCTP(addr,
+		sctp.WithAddressFamily(addr.AddressFamily),
+		sctp.WithNonBlocking(false),
+		sctp.WithMode(types.OneToOne),
+		sctp.WithInitMsg(types.InitMsg{}))
+	if err != nil {
+		return nil, err
+	}
+	channel := channels.NewE2NodeChannel(conn, func(channel channels.E2NodeChannel) procedures.E2NodeProcedures {
+		return handler(channel)
+	})
+	return channel, nil
+}
