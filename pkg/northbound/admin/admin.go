0e1fccc8a892dff90348fc9e230dc30b0bff0562
Jordan Halterman <jordan.halterman@gmail.com>
2021-09-07 19:03:53 -0700

    Return NotSupported errors from admin API calls

--- pkg/northbound/admin/admin.go
+++ pkg/northbound/admin/admin.go
@@ -7,7 +7,6 @@ package admin
 import (
 	"context"
 	e2server "github.com/onosproject/onos-e2t/pkg/southbound/e2ap/server"
-	"github.com/onosproject/onos-e2t/pkg/southbound/e2ap/types"
 	"github.com/onosproject/onos-lib-go/pkg/errors"
 
 	adminapi "github.com/onosproject/onos-api/go/onos/e2t/admin"
@@ -47,15 +46,12 @@ type Server struct {
 
 // UploadRegisterServiceModel uploads and adds the model plugin to the list of supported models
 func (s *Server) UploadRegisterServiceModel(stream adminapi.E2TAdminService_UploadRegisterServiceModelServer) error {
-	log.Error("implement me")
-
-	return nil
+	return errors.Status(errors.NewNotSupported("UploadRegisterServiceModel not implemented")).Err()
 }
 
 // ListRegisteredServiceModels returns a stream of registered service models.
 func (s *Server) ListRegisteredServiceModels(req *adminapi.ListRegisteredServiceModelsRequest, stream adminapi.E2TAdminService_ListRegisteredServiceModelsServer) error {
-	log.Error("implement me")
-	return nil
+	return errors.Status(errors.NewNotSupported("ListRegisteredServiceModels not implemented")).Err()
 }
 
 // ListE2NodeConnections returns a stream of existing SCTP connections.
@@ -63,23 +59,7 @@ func (s *Server) ListE2NodeConnections(req *adminapi.ListE2NodeConnectionsReques
 	return errors.Status(errors.NewNotSupported("ListE2NodeConnections not supported: use onos-topo instead")).Err()
 }
 
-func connectionType(nodeType types.E2NodeType) adminapi.E2NodeConnectionType {
-	switch nodeType {
-	case types.E2NodeTypeGNB:
-		return adminapi.E2NodeConnectionType_G_NB
-	case types.E2NodeTypeENB:
-		return adminapi.E2NodeConnectionType_E_NB
-	case types.E2NodeTypeEnGNB:
-		return adminapi.E2NodeConnectionType_ENG_MB
-	case types.E2NodeTypeNgENB:
-		return adminapi.E2NodeConnectionType_NGE_NB
-	default:
-		return adminapi.E2NodeConnectionType_G_NB
-	}
-}
-
 // DropE2NodeConnections drops the specified E2 node SCTP connections
 func (s *Server) DropE2NodeConnections(ctx context.Context, req *adminapi.DropE2NodeConnectionsRequest) (*adminapi.DropE2NodeConnectionsResponse, error) {
-	log.Error("implement me")
-	return &adminapi.DropE2NodeConnectionsResponse{}, nil
+	return nil, errors.Status(errors.NewNotSupported("DropE2NodeConnections not supported: use onos-topo instead")).Err()
 }
