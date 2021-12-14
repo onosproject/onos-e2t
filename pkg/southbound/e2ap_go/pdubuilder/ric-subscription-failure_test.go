e7f5ea40a36123296d290b69238a1c9bc2c3661e
Ivan Eroshkin <erijj@mail.ru>
2021-12-14 11:45:02 +0100

    Adding one more unit test

--- pkg/southbound/e2ap_go/pdubuilder/ric-subscription-failure_test.go
+++ pkg/southbound/e2ap_go/pdubuilder/ric-subscription-failure_test.go
@@ -5,6 +5,8 @@ package pdubuilder
 
 import (
 	"encoding/hex"
+	v21 "github.com/onosproject/onos-e2t/api/e2ap/v2"
+	e2apcommondatatypes "github.com/onosproject/onos-e2t/api/e2ap/v2/e2ap-commondatatypes"
 	e2ap_ies "github.com/onosproject/onos-e2t/api/e2ap/v2/e2ap-ies"
 	"github.com/onosproject/onos-e2t/pkg/southbound/e2ap/asn1cgo"
 	"github.com/onosproject/onos-e2t/pkg/southbound/e2ap/pdubuilder"
@@ -20,6 +22,49 @@ import (
 )
 
 func TestRicSubscriptionFailure(t *testing.T) {
+	ricActionsNotAdmittedList1 := make(map[types1.RicActionID]*e2ap_ies.Cause)
+	ricActionsNotAdmittedList1[100] = &e2ap_ies.Cause{
+		Cause: &e2ap_ies.Cause_Transport{
+			Transport: e2ap_ies.CauseTransport_CAUSE_TRANSPORT_TRANSPORT_RESOURCE_UNAVAILABLE,
+		},
+	}
+	ricActionsNotAdmittedList1[200] = &e2ap_ies.Cause{
+		Cause: &e2ap_ies.Cause_Misc{
+			Misc: e2ap_ies.CauseMisc_CAUSE_MISC_HARDWARE_FAILURE,
+		},
+	}
+
+	procCode1 := v21.ProcedureCodeIDRICsubscription
+	criticality1 := e2apcommondatatypes.Criticality_CRITICALITY_IGNORE
+	ftg1 := e2apcommondatatypes.TriggeringMessage_TRIGGERING_MESSAGE_UNSUCCESSFUL_OUTCOME
+
+	e2apPdu, err := pdubuilder.CreateRicSubscriptionFailureE2apPdu(&types1.RicRequest{
+		RequestorID: 22,
+		InstanceID:  6,
+	}, 9, &e2ap_ies.Cause{
+		Cause: &e2ap_ies.Cause_Misc{
+			Misc: e2ap_ies.CauseMisc_CAUSE_MISC_CONTROL_PROCESSING_OVERLOAD,
+		},
+	})
+	assert.NilError(t, err)
+	assert.Assert(t, e2apPdu != nil)
+	e2apPdu.GetUnsuccessfulOutcome().GetProcedureCode().GetRicSubscription().GetUnsuccessfulOutcome().
+		SetCriticalityDiagnostics(&procCode1, &criticality1, &ftg1,
+			&types1.RicRequest{
+				RequestorID: 10,
+				InstanceID:  20,
+			}, []*types1.CritDiag{
+				{
+					TypeOfError:   e2ap_ies.TypeOfError_TYPE_OF_ERROR_MISSING,
+					IECriticality: e2apcommondatatypes.Criticality_CRITICALITY_IGNORE,
+					IEId:          v21.ProtocolIeIDRicsubscriptionDetails,
+				},
+			})
+
+	per, err := asn1cgo.PerEncodeE2apPdu(e2apPdu)
+	assert.NilError(t, err)
+	t.Logf("RicSubscriptionFailure E2AP PDU PER\n%v", hex.Dump(per))
+
 	ricActionsNotAdmittedList := make(map[types.RicActionID]*e2apies.Cause)
 	ricActionsNotAdmittedList[100] = &e2apies.Cause{
 		Cause: &e2apies.Cause_Transport{
@@ -64,20 +109,16 @@ func TestRicSubscriptionFailure(t *testing.T) {
 	t.Logf("RicSubscriptionFailure E2AP PDU PER with Go APER library\n%v", hex.Dump(perNew))
 
 	//Comparing reference PER bytes with Go APER library produced
-	//assert.DeepEqual(t, per, perNew)
+	assert.DeepEqual(t, per, perNew)
 
 	//e2apPdu, err := encoder.PerDecodeE2ApPdu(perNew)
 	//assert.NilError(t, err)
 	//assert.DeepEqual(t, newE2apPdu.String(), e2apPdu.String())
 
-	//per, err := asn1cgo.PerEncodeE2apPdu(newE2apPdu)
-	//assert.NilError(t, err)
-	//t.Logf("RicSubscriptionFailure E2AP PDU PER\n%v", hex.Dump(per))
-	//
-	//result1, err := asn1cgo.PerDecodeE2apPdu(per)
-	//assert.NilError(t, err)
-	//t.Logf("RicSubscriptionFailure E2AP PDU PER - decoded\n%v\n", result1)
-	//assert.DeepEqual(t, newE2apPdu.String(), result1.String())
+	result1, err := asn1cgo.PerDecodeE2apPdu(perNew)
+	assert.NilError(t, err)
+	t.Logf("RicSubscriptionFailure E2AP PDU PER - decoded\n%v\n", result1)
+	assert.DeepEqual(t, e2apPdu.String(), result1.String())
 }
 
 func TestRicSubscriptionFailureExcludeOptionalIE(t *testing.T) {
