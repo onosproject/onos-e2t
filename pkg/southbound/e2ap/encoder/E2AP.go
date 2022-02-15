10200cbd23a84837dcec6257263a24505773eef8
Ivan Eroshkin <erijj@mail.ru>
2022-02-15 17:20:09 +0100

    Updated logger invoking

--- pkg/southbound/e2ap/encoder/E2AP.go
+++ pkg/southbound/e2ap/encoder/E2AP.go
@@ -6,25 +6,16 @@ package encoder
 
 import (
 	"encoding/hex"
-	"github.com/google/martian/log"
 	e2appdudescriptions "github.com/onosproject/onos-e2t/api/e2ap/v2/e2ap-pdu-descriptions"
 	"github.com/onosproject/onos-lib-go/pkg/asn1/aper"
 	"github.com/onosproject/onos-lib-go/pkg/logging"
 )
 
-func init() {
-	log := logging.GetLogger("asn1")
-	log.SetLevel(logging.InfoLevel)
-}
+var log = logging.GetLogger("southbound", "e2ap", "encoder")
 
 func PerEncodeE2ApPdu(e2ap *e2appdudescriptions.E2ApPdu) ([]byte, error) {
 
 	log.Debugf("Obtained E2AP-PDU message is\n%v", e2ap)
-	//choicemap = e2appdudescriptions.E2ApPduChoicemap
-	//canonicalChoiceMap = e2appdudescriptions.E2ApPduCanonicalChoicemap
-
-	//log.Debugf("CHOICE map is following:\n%v", aper.ChoiceMap)
-	//log.Debugf("Canonical CHOICE map is following:\n%v", aper.CanonicalChoiceMap)
 
 	per, err := aper.MarshalWithParams(e2ap, "choiceExt", e2appdudescriptions.E2ApPduChoicemap, e2appdudescriptions.E2ApPduCanonicalChoicemap)
 	if err != nil {
@@ -38,11 +29,6 @@ func PerEncodeE2ApPdu(e2ap *e2appdudescriptions.E2ApPdu) ([]byte, error) {
 func PerDecodeE2ApPdu(per []byte) (*e2appdudescriptions.E2ApPdu, error) {
 
 	log.Debugf("Obtained E2AP-PDU PER bytes are\n%v", hex.Dump(per))
-	//choicemap = e2appdudescriptions.E2ApPduChoicemap
-	//canonicalChoiceMap = e2appdudescriptions.E2ApPduCanonicalChoicemap
-
-	//log.Debugf("CHOICE map is following:\n%v", aper.ChoiceMap)
-	//log.Debugf("Canonical CHOICE map is following:\n%v", aper.CanonicalChoiceMap)
 
 	result := e2appdudescriptions.E2ApPdu{}
 	err := aper.UnmarshalWithParams(per, &result, "choiceExt", e2appdudescriptions.E2ApPduChoicemap, e2appdudescriptions.E2ApPduCanonicalChoicemap)
