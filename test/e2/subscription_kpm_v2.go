387d2ac04802130dbe514ec5f1822188ce4828b4
Ray Milkey <ray@opennetworking.org>
2022-03-31 12:02:14 -0700

    Refactor KPM V2 subsbcriptions to use common types (#574)
    * Refactor KPM V2 subsbcriptions to use common types
    
    * gofmt
    
    * Address some review comments
    
    - removed context from shared structure, now passed in to individual calls
    - refactor shared fields into a separate structure

--- test/e2/subscription_kpm_v2.go
+++ test/e2/subscription_kpm_v2.go
@@ -8,13 +8,10 @@ import (
 	"context"
 	"testing"
 
-	"github.com/onosproject/onos-api/go/onos/e2t/e2/v1beta1"
-	e2api "github.com/onosproject/onos-api/go/onos/e2t/e2/v1beta1"
 	e2smkpmv2 "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm_v2_go/v2/e2sm-kpm-v2-go"
 	"github.com/onosproject/onos-e2t/test/e2utils"
 	"google.golang.org/protobuf/proto"
 
-	sdkclient "github.com/onosproject/onos-ric-sdk-go/pkg/e2/v1beta1"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/onosproject/onos-e2t/test/utils"
@@ -36,66 +33,42 @@ func (s *TestSuite) TestSubscriptionKpmV2(t *testing.T) {
 	cells, err := topoSdkClient.GetCells(ctx, nodeID)
 	assert.NoError(t, err)
 
-	reportPeriod := uint32(5000)
-	granularity := uint32(500)
-
-	// Kpm v2 interval is defined in ms
-	eventTriggerBytes, err := utils.CreateKpmV2EventTrigger(reportPeriod)
-	assert.NoError(t, err)
+	subName := "TestSubscriptionKpmV2"
 
 	// Use one of the cell object IDs for action definition
 	cellObjectID := cells[0].CellObjectID
-	actionDefinitionBytes, err := utils.CreateKpmV2ActionDefinition(cellObjectID, granularity)
-	assert.NoError(t, err)
 
-	var actions []e2api.Action
-	action := e2api.Action{
-		ID:   100,
-		Type: e2api.ActionType_ACTION_TYPE_REPORT,
-		SubsequentAction: &e2api.SubsequentAction{
-			Type:       e2api.SubsequentActionType_SUBSEQUENT_ACTION_TYPE_CONTINUE,
-			TimeToWait: e2api.TimeToWait_TIME_TO_WAIT_ZERO,
+	// Create a KPM V2 subscription
+	kpmv2Sub := e2utils.KPMV2Sub{
+		Sub: e2utils.Sub{
+			Name:   subName,
+			NodeID: nodeID,
 		},
-		Payload: actionDefinitionBytes,
+		CellObjectID: cellObjectID,
 	}
-
-	actions = append(actions, action)
-
-	subRequest := utils.Subscription{
-		NodeID:              string(nodeID),
-		EventTrigger:        eventTriggerBytes,
-		ServiceModelName:    utils.KpmServiceModelName,
-		ServiceModelVersion: utils.Version2,
-		Actions:             actions,
-	}
-
-	subSpec, err := subRequest.CreateWithActionDefinition()
+	_, err = kpmv2Sub.Subscribe(ctx)
 	assert.NoError(t, err)
 
-	subName := "TestSubscriptionKpmV2"
+	// Read an indication
+	indicationReport := e2utils.CheckIndicationMessage(t, e2utils.DefaultIndicationTimeout, kpmv2Sub.Sub.Ch)
 
-	sdkClient := utils.GetE2Client(t, utils.KpmServiceModelName, utils.Version2, sdkclient.ProtoEncoding)
-	node := sdkClient.Node(sdkclient.NodeID(nodeID))
-	ch := make(chan v1beta1.Indication)
-	_, err = node.Subscribe(ctx, subName, subSpec, ch)
-	assert.NoError(t, err)
-
-	indicationReport := e2utils.CheckIndicationMessage(t, e2utils.DefaultIndicationTimeout, ch)
+	// Check the format of the indiction message
 	indicationMessage := e2smkpmv2.E2SmKpmIndicationMessage{}
-	indicationHeader := e2smkpmv2.E2SmKpmIndicationHeader{}
-
-	err = proto.Unmarshal(indicationReport.Payload, &indicationMessage)
-	assert.NoError(t, err)
+	assert.NoError(t, proto.Unmarshal(indicationReport.Payload, &indicationMessage))
 	indMsgFormat1 := indicationMessage.GetIndicationMessageFormats().GetIndicationMessageFormat1()
 	assert.Equal(t, indMsgFormat1.GetCellObjId().Value, cellObjectID)
-	assert.Equal(t, int(reportPeriod/granularity), len(indMsgFormat1.GetMeasData().GetValue()))
+	assert.Equal(t, int(kpmv2Sub.ReportPeriod/kpmv2Sub.Granularity), len(indMsgFormat1.GetMeasData().GetValue()))
 
-	err = proto.Unmarshal(indicationReport.Header, &indicationHeader)
-	assert.NoError(t, err)
+	// Check the format of the indication header
+	indicationHeader := e2smkpmv2.E2SmKpmIndicationHeader{}
+	assert.NoError(t, proto.Unmarshal(indicationReport.Header, &indicationHeader))
+	format1 := indicationHeader.IndicationHeaderFormats.E2SmKpmIndicationHeader.(*e2smkpmv2.IndicationHeaderFormats_IndicationHeaderFormat1)
+	assert.NotNil(t, format1)
+	assert.Equal(t, "ONF", *format1.IndicationHeaderFormat1.VendorName)
+	assert.Equal(t, "RAN Simulator", *format1.IndicationHeaderFormat1.SenderName)
 
-	err = node.Unsubscribe(ctx, subName)
-	assert.NoError(t, err)
-
+	// Clean up
+	assert.NoError(t, kpmv2Sub.Unsubscribe(ctx))
 	e2utils.CheckForEmptySubscriptionList(t)
 	utils.UninstallRanSimulatorOrDie(t, sim)
 }
